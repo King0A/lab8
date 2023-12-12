f35508ac83644d53c1a1c078a4b2e7e8c2cd598d
King0A <sptt00.kingkiller@gmail.com>
2023-12-12 16:00:54 +0700

    FN2

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+string findGrade(double grade)
+{
+  float g;
+  if (g==1)
+  {cout << "1";}
+    else{if (g==2)
+    {cout << "2";}
+      else{if (g==3)
+      {cout << "3";}
+        else{if (g==4)
+        {cout << "4";}
+          else{if (g==5)
+          {cout << "5";}
+          }
+        }
+      }
+    }
+  return 0;
+}
